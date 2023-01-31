25d2dfe5ad6e2d094ae16b9ff523dc3af476a10e
luccskywalker <lucasemanuelalves@hotmail.com>
2023-01-31 15:26:22 -0300

    Card component and holo effect

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,15 @@
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, Input, OnInit, Renderer2 } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.less'],
+})
+export class CardComponent implements OnInit {
+  @Input() img!: string | undefined;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/pull/pull.module.ts
+++ src/app/pull/pull.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PullComponent } from './pull.component';
+import { CardModule } from '../card/card.module';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, CardModule],
   declarations: [PullComponent],
   providers: [PullComponent],
   exports: [PullComponent],
