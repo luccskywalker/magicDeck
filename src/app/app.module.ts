3160fc2b4e60b127b96673e1ca36596c765af14e
luccskywalker <lucasemanuelalves@hotmail.com>
2023-02-07 17:09:58 -0300

    Home Page oil drops

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
 import { PullModule } from './pull/pull.module';
+import { HomeComponent } from './home/home.component';
+import { PullComponent } from './pull/pull.component';
 
+const appRoutes: Routes = [
+  { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: 'home', pathMatch: 'full', component: HomeComponent },
+  { path: 'pull', pathMatch: 'full', component: PullComponent },
+];
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, PullModule],
+  imports: [
+    BrowserModule,
+    PullModule,
+    HomeModule,
+    RouterModule.forRoot(appRoutes),
+  ],
   providers: [AppModule],
   bootstrap: [AppComponent],
+  exports: [RouterModule],
 })
 export class AppModule {}
