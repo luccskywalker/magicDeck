80703dce080652dacf95feda95f2c81559e77f23
Lucas Emmanuel <lucasemanuelalves@hotmail.com>
2023-02-08 00:09:43 -0300

    First router

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { AppModule } from '../app.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   declarations: [HomeComponent],
   exports: [HomeComponent],
 })
